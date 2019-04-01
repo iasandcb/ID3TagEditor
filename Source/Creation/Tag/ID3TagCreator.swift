852354624336953fedb319334b32253c93fb58a1
Changshin Lee <iasandcb@gmail.com>
2019-04-01 23:42:45 +0900

    Make ID3TagCreator.create public

--- Source/Creation/Tag/ID3TagCreator.swift
+++ Source/Creation/Tag/ID3TagCreator.swift
@@ -16,7 +16,7 @@ public class ID3TagCreator {
         self.id3TagHeaderCreator = id3TagHeaderCreator
     }
 
-    func create(id3Tag: ID3Tag) throws -> Data {
+    public func create(id3Tag: ID3Tag) throws -> Data {
         let frames = try id3FramesCreator.createFramesUsing(id3Tag: id3Tag)
         let header = id3TagHeaderCreator.createTagHeader(contentSize: frames.count, id3Tag: id3Tag)
         let tag = header + frames
